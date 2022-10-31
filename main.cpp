ca470926e768c14245558cb1d97372474d13bd41
kewldan <40865857+kewldan@users.noreply.github.com>
2022-10-31 23:37:06 +0300

    Shadows #1

--- main.cpp
+++ main.cpp
@@ -23,6 +23,7 @@ int main() {
     window->setVsync(false);
 
     auto *shader = new Shader("main");
+    auto *simpleDepthShader = new Shader("depth");
     int projection_location = shader->getUniformLocation("proj");
 
     auto *sniperRifle = new Model("./data/meshes/sniper.obj", shader);
@@ -39,6 +40,25 @@ int main() {
 
     unsigned long lastFps = millis();
     int fps, fpsCounter;
+
+    unsigned int depthMapFBO;
+    glGenFramebuffers(1, &depthMapFBO);
+    const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
+    unsigned int depthMap;
+    glGenTextures(1, &depthMap);
+    glBindTexture(GL_TEXTURE_2D, depthMap);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT,
+                 SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
+    glDrawBuffer(GL_NONE);
+    glReadBuffer(GL_NONE);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
     auto *camera = new Camera();
     while (window->update()) {
@@ -78,23 +98,44 @@ int main() {
             camera->position.z -= std::sin(camera->rotation.y) * 0.03f;
         }
 
-        sniperRifle->rotation.y = glfwGetTime();
-        sniperRifle->position.y = std::sin(glfwGetTime() * 2);
+        sniperRifle->rotation.y = (float) glfwGetTime();
+        sniperRifle->position.y = (float) std::sin(glfwGetTime() * 2);
 
         monkey->position.x = 3;
-        monkey->rotation.y = glfwGetTime();
+        monkey->rotation.y = (float) glfwGetTime();
 
+        glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+        glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+        glClear(GL_DEPTH_BUFFER_BIT);
+        float near_plane = 1.0f, far_plane = 7.5f;
+        glm::mat4 lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
+        glm::mat4 lightView = glm::lookAt(glm::vec3(-2.0f, 4.0f, -1.0f),
+                                          glm::vec3(0.0f, 0.0f, 0.0f),
+                                          glm::vec3(0.0f, 1.0f, 0.0f));
+        glm::mat4 lightSpaceMatrix = lightProjection * lightView;
+        simpleDepthShader->bind();
+        glUniformMatrix4fv(simpleDepthShader->getUniformLocation("lightSpaceMatrix"), 1, GL_FALSE,
+                           glm::value_ptr(lightSpaceMatrix));
+        sniperRifle->draw(shader);
+        floor->draw(shader);
+        monkey->draw(shader);
+        simpleDepthShader->unbind();
+
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glViewport(0, 0, window->getWidth(), window->getHeight());
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         shader->bind();
         glUniformMatrix4fv(projection_location, 1, false, glm::value_ptr(*window->getProj()));
         shader->upload(camera);
-
         sniperRifle->draw(shader);
-
         floor->draw(shader);
-
         monkey->draw(shader);
+        shader->unbind();
+
 
         window->end();
+
+
         glfwSetWindowTitle(window->getId(), (std::string("FPS: ") + std::to_string(fps)).c_str());
         fpsCounter++;
     }
